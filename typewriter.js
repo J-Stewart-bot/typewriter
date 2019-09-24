f372c0ce208e47ef52640c6a5d9fe47cfc2c2fe3
Justin Stewart <Justin.lighthouse.labs@gmail.com>
2019-09-24 19:49:38 +0000

    completed

---
+++ typewriter.js
@@ -0,0 +1,7 @@
+const sentence = "hello there from lighthouse labs";
+let offSet = 50
+for (const char of sentence) {
+  setTimeout(() => { process.stdout.write(char) }, offSet);
+  offSet += 50;
+}
+setTimeout(() => { console.log() }, 1750);
